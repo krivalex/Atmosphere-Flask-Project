1890816452b95f858e07b9aeb19b2187d6379722
krivalex <aleksei_krivoborodov@mail.ru>
2023-03-24 22:36:03 +0600

    make crud system

--- index.py
+++ index.py
@@ -13,6 +13,58 @@ def calculate():
     return render_template('calculate.html', posts=posts)
 
 
+@app.route('/crud')
+def crud():
+    return render_template('crud.html')
+
+
+@app.route('/crud/get', methods=['GET', 'POST'])
+def crud_get():
+    id = request.args.get('id')
+    for post in posts:
+        if str(post['id']) == str(id):
+            return render_template('crud.html', post=post)
+    return "Post not found"
+
+
+@app.route('/crud/delete', methods=['GET', 'POST'])
+def crud_delete():
+    id = request.args.get('id')
+    for post in posts:
+        if str(post['id']) == str(id):
+            posts.remove(post)
+            return "Post deleted " + str(id)
+    return "Post not found"
+
+
+@app.route('/crud/put', methods=['GET', 'POST'])
+def crud_put():
+    id = request.args.get('id')
+    for post in posts:
+        if str(post['id']) == str(id):
+            post['author'] = "Updated"
+            post['title'] = "Updated"
+            post['content'] = "Updated"
+            return "Post updated " + str(id)
+    return "Post not found"
+
+
+@app.route('/crud/post', methods=['POST'])
+def crud_post():
+    if request.method == 'POST':
+        id = request.form['id']
+        for post in posts:
+            if str(post['id']) == str(id):
+                return "Post already exists " + str(id)
+        new_post = {}
+        new_post['author'] = "Posted"
+        new_post['title'] = "Posted"
+        new_post['content'] = "Posted"
+        new_post['id'] = str(id)
+        posts.append(new_post)
+        return "Post posted " + str(id)
+
+
 @app.route('/add_card', methods=['POST'])
 def add_card():
     # Get the data from the form using request.form
